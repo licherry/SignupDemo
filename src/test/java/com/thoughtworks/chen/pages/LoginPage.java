e552f711968a1013417fc49d8dfb585d7a5ac86c
chenli <382782960@qq.com>
2016-04-22 16:19:54 +0800

    [Chen] modify some files.

--- src/test/java/com/thoughtworks/chen/pages/LoginPage.java
+++ src/test/java/com/thoughtworks/chen/pages/LoginPage.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 /**
- * Created by chenli on 4/20/16.
+ * Created by chenli on 4/22/16.
  */
-public class SignInPage {
+public class LoginPage {
     WebDriver driver;
 
     @FindBy(id = "user_email")
@@ -20,7 +20,7 @@ public class SignInPage {
     @FindBy(name = "commit")
     private WebElement signinButton;
 
-    public SignInPage(WebDriver driver){
+    public LoginPage(WebDriver driver){
         this.driver=driver;
         PageFactory.initElements(driver,this);
     }
